0b4e19c2453e6c9a2c7364261b999fc380bd4024
Adrian Iten <adrian.iten@competec.ch>
2022-09-15 10:46:56 +0200

    Thesis visualizations

--- run_visualize_samples.py
+++ run_visualize_samples.py
@@ -16,8 +16,20 @@ LOOP_THREES = [841, 3246, 43035, 43741, 44253]
 LOOP_FOURS = [842, 3514, 42862, 44003, 47927]
 LOOP_ENDS = [899, 1299, 2785, 43366, 44228]
 
+EMPTIES_ORT = [657, 30251, 30622, 35213, 36340]
+ARROWS_DOWN = [944, 2987, 8321, 44503, 45557]
+ARROWS_RIGHT = [1641, 46901, 43474, 3180, 2305]
+ARROWS_UP = [1467, 1779, 3906, 43455, 47915]
+ARROWS_LEFT = [1219, 3069, 31659, 43630, 44844]
+
+EMPTIES_CRS = [193, 1440, 1730, 47579, 35789]
+CROSSES = [947, 1627, 2102, 2446, 33667]
+
 BASIC_INSTRUCTION_INDICES = EMPTIES + PLUS_ONES + MINUS_ONES + TURN_RIGHTS + TURN_LEFTS
 ADVANCED_INSTRUCTION_INDICES = LOOP_TWICES + LOOP_THREES + LOOP_FOURS + LOOP_ENDS
+
+ORIENTATION_INDICES = EMPTIES_ORT + ARROWS_DOWN + ARROWS_RIGHT + ARROWS_UP + ARROWS_LEFT
+CROSS_INDICES = EMPTIES_CRS + CROSSES
 
 
 def plot_samples(full_df, indices, n_cols, n_rows):
@@ -49,3 +61,12 @@ if __name__ == "__main__":
 
     fig2 = plot_samples(df, ADVANCED_INSTRUCTION_INDICES, 5, 4)
     fig2.savefig('./output_visualizations/observations_in_advanced_instructions.pdf')
+    plt.show()
+
+    fig3 = plot_samples(df, ORIENTATION_INDICES, 5, 5)
+    fig3.savefig('./output_visualizations/observations_in_orientation.pdf')
+    plt.show()
+
+    fig4 = plot_samples(df, CROSS_INDICES, 5, 2)
+    fig4.savefig('./output_visualizations/observations_in_crosses.pdf')
+    plt.show()
