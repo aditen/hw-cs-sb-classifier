0ee3c3a78d45bb8fab411410db51feb516f5ff3a
Adrian Iten <adrian.iten@competec.ch>
2022-09-17 12:25:34 +0200

    Add LeNet from Torch Tutorials

--- grayscale_model.py
+++ grayscale_model.py
@@ -1,21 +1,51 @@
 from enum import Enum
 
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
 class ModelVersion(Enum):
-    XS = 8
     SM = 4
-    MD = 2
     LG = 1
-    LE_NET_PP = 0
+    LE_NET_PP = -1
+    LE_NET = -2
 
 
 def get_model(model_version: ModelVersion, num_classes):
     if model_version == ModelVersion.LE_NET_PP:
         return LeNetPP(num_classes)
+    elif model_version == ModelVersion.LE_NET:
+        return LeNet(n_classes=num_classes)
     return SimpleNet(version=model_version, classes=num_classes)
+
+
+# adapted to n_classes
+# from PyTorch Tutorial: https://pytorch.org/tutorials/beginner/blitz/neural_networks_tutorial.html
+class LeNet(nn.Module):
+
+    def __init__(self, n_classes=10):
+        super(LeNet, self).__init__()
+        # 1 input image channel, 6 output channels, 5x5 square convolution
+        # kernel
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)  # 5*5 from image dimension
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, n_classes)
+
+    def forward(self, x):
+        # Max pooling over a (2, 2) window
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        # If the size is a square, you can specify with a single number
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = torch.flatten(x, 1)  # flatten all dimensions except the batch dimension
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        # we do not do any Space visualization here!
+        return x, None
 
 
 # LeNet++ from Center Loss Paper, see https://github.com/KaiyangZhou/pytorch-center-loss/blob/master/models.py
@@ -83,7 +113,6 @@ class SimpleNet(nn.Module):
         return out, out_2d
 
     def _make_layers(self, in_channels=1, channel_divisor=1):
-
         n_channels_to_int = int(64 / channel_divisor)
         model = nn.Sequential(
             nn.Conv2d(in_channels, n_channels_to_int, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
@@ -153,7 +182,7 @@ class SimpleNet(nn.Module):
             nn.BatchNorm2d(n_channels_to_int * 4, eps=1e-05, momentum=0.05, affine=True),
             nn.ReLU(inplace=True),
 
-            # NOTE: global max pooling would be before this layer for bigger input sizes
+            # NOTE: global max pooling would be before this layer for bigger input sizes, but for 32x32 it comes to 1x1
             nn.Flatten(),
             nn.Dropout(0.1),
             nn.Linear(n_channels_to_int * 4, 2),
