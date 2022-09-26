11ada3ea2e8062df6d90b693d5d03e295294f0f8
Adrian Iten <adrian.iten@competec.ch>
2022-09-26 20:54:56 +0200

    Update baseline experiment

--- run_baseline.py
+++ run_baseline.py
@@ -4,7 +4,6 @@ from tabulate import tabulate
 from data_augmentation import DataAugmentationOptions
 from data_loading import DataloaderKinderlabor, TaskType, DataSplit
 from grayscale_model import ModelVersion
-from run_utils import RunUtilsKinderlabor
 from training import TrainerKinderlabor
 from visualizing import VisualizerKinderlabor, data_split_dict, short_names_models, short_names_tasks
 
@@ -19,16 +18,16 @@ run_configs = [
 ]
 
 if __name__ == "__main__":
+    data_arr = []
     for task_type in TaskType:
-        data_arr = []
         for run_config in run_configs:
             for data_split in DataSplit:
                 for model in [ModelVersion.SM, ModelVersion.LE_NET]:
-                    run_id = f"base_task[{short_names_tasks[task_type]}]_aug[{run_config[0]}]_split[{data_split_dict[data_split]}]_" \
-                             f"model[{short_names_models[model]}]"
+                    run_id = f"base_task[{short_names_tasks[task_type]}]_aug[{run_config[0]}]" \
+                             f"_split[{data_split_dict[data_split]}]_model[{short_names_models[model]}]"
                     print(
                         f'Running for configuration {run_config[0]} and data split {data_split.value} '
-                        f'using model {model.name}')
+                        f'and task {task_type.name} using model {model.name}')
 
                     # Initialize data loader: data splits and loading from images from disk
                     loader_orientation = DataloaderKinderlabor(task_type=task_type,
@@ -42,7 +41,7 @@ if __name__ == "__main__":
                     trainer_orientation = TrainerKinderlabor(loader_orientation, load_model_from_disk=True,
                                                              run_id=run_id,
                                                              model_version=model)
-                    trainer_orientation.train_model(n_epochs=50)
+                    trainer_orientation.train_model(n_epochs=75)
                     visualizer_orientation.visualize_training_progress(trainer_orientation)
 
                     # Predict on test samples
@@ -51,12 +50,13 @@ if __name__ == "__main__":
                     trainer_orientation.script_model()
 
                     data_arr.append(
-                        [run_config[0], data_split_dict[data_split], trainer_orientation.get_predictions()[6],
-                         model.name])
-        print(tabulate([[x[1], x[3], x[0], f'{(x[2] * 100):.2f}%'] for x in data_arr],
-                       headers=['Data Split', 'Model', 'Augmentations', 'Performance']))
-        df = pd.DataFrame.from_dict({'augmentation': [row[0] for row in data_arr],
-                                     'split': [row[1] for row in data_arr],
-                                     'performance': [row[2] for row in data_arr],
-                                     'model': [row[3] for row in data_arr]})
-        df.to_csv(f'./output_visualizations/runs_base_aug[{short_names_tasks[task_type]}].csv', sep=';')
+                        [run_config[0], data_split_dict[data_split], short_names_tasks[task_type],
+                         trainer_orientation.get_predictions()[6], model.name])
+    print(tabulate([[x[1], x[2], x[4], x[0], f'{(x[3] * 100):.2f}%'] for x in data_arr],
+                   headers=['Data Split', 'Task', 'Model', 'Augmentation', 'Performance']))
+    df = pd.DataFrame.from_dict({'augmentation': [row[0] for row in data_arr],
+                                 'split': [row[1] for row in data_arr],
+                                 'task': [row[2] for row in data_arr],
+                                 'performance': [row[3] for row in data_arr],
+                                 'model': [row[4] for row in data_arr]})
+    df.to_csv(f'./output_visualizations/runs_base.csv', sep=';')
